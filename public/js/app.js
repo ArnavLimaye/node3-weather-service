f1b831e13f72d1f892ab2b2d1a0e37104e962917
Arnav <aal4493@gmail.com>
2020-02-26 12:21:45 +0530

    Made changes to configure app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) =>{
     weatherMessage.textContent = '';
 
     const location = search.value;
-    fetch('http://localhost:3000/weather?address='+location).then((response => {
+    fetch('/weather?address='+location).then((response => {
         response.json().then((data) => {
             if(data.error){
                 errorMessage.textContent = data.error;
